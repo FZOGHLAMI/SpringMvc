8d6534ddb2c54dfd84bf9b75d7ea8b36cad1e7d1
ZO <zoghlamifawzi@gmail.com>
2018-09-18 20:15:56 +0200

    add chercher produit par mot clé  add spring-boot-devtools

--- src/main/java/com/aviva/board/controller/ProduitController.java
+++ src/main/java/com/aviva/board/controller/ProduitController.java
@@ -3,9 +3,12 @@ package com.aviva.board.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.aviva.board.model.Produit;
 import com.aviva.board.repository.ProduitRepository;
@@ -18,9 +21,19 @@ public class ProduitController {
 	private ProduitRepository produitRepository;
 	
 	@RequestMapping(value="/index")
-	public String index(Model model) {
-		List<Produit> liste=produitRepository.findAll();
-		model.addAttribute("listeProduits",liste);
+	public String index(Model model, @RequestParam(name="page",defaultValue="0") int p , @RequestParam(name="size",defaultValue="5") int s, @RequestParam(name="motCle",defaultValue="")String mc) {
+		//Page<Produit> pageProduits=produitRepository.findAll(new PageRequest(page,s));
+		//Page<Produit> pageProduits=produitRepository.findAll(PageRequest.of(p,s));
+		Page<Produit> pageProduits=produitRepository.chercher("%"+mc+"%", PageRequest.of(p,s));
+		
+		
+		model.addAttribute("listeProduits",pageProduits.getContent());
+        int[] pages= new int[pageProduits.getTotalPages()];
+    	model.addAttribute("pages",pages);
+    	model.addAttribute("pageCourante",p);
+    	model.addAttribute("size",s);
+    	model.addAttribute("motCle",mc);
+    	
 		return "produits";//retourne vue produits.html
 	}
 }
--- src/main/java/com/aviva/board/repository/ProduitRepository.java
+++ src/main/java/com/aviva/board/repository/ProduitRepository.java
@@ -1,6 +1,11 @@
 package com.aviva.board.repository;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.aviva.board.model.Produit;
@@ -9,7 +14,7 @@ import com.aviva.board.model.Role;
 
 @Repository("produitRepository")
 public interface ProduitRepository extends JpaRepository<Produit, Long>{
-	
-	//Role findByRole(String role);
 
+	@Query("select p from Produit p where p.designation like :x")
+	public Page<Produit> chercher(@Param("x") String motCle, Pageable pageable);
 }
